8c40af90d5411dfb8d7af7ea01d032d1c111836c
BenjaminRandolph <114035117+BenjaminRandolph@users.noreply.github.com>
2025-05-02 22:11:18 -0700

    Update Signup.tsx

--- Front End (React)/src/Signup.tsx
+++ Front End (React)/src/Signup.tsx
@@ -36,7 +36,7 @@ function Signup(){
 		}
 	
 		try {
-		  const response = await fetch('https://cs360labbackend.railway.internal8080/api/User', {
+		  const response = await fetch('https://' + process.env.API_URL + '/api/User', {
 			method: 'POST',
 			headers: {
 			  'Content-Type': 'application/json',
